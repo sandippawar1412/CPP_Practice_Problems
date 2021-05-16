c3e526aa98a41ee0279e5854b3041acb31acbb03
Sandeep Pawar <sandippawar1412@gmail.com>
2021-05-16 17:23:01 +0530

    vec value change

--- testApp/main.cpp
+++ testApp/main.cpp
@@ -8,6 +8,7 @@ int main(){
     cout<<"Hello World"<<std::endl;
     cout<<"Hello World"<<std::endl;
     vector<int> vec = {10};
+    vec.push_back(100);
     cout<<"Hello World"<<std::endl;
-    
+    cout<<"size: "<<vec.size()<<std::endl;
 }
