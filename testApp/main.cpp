8d019abe2ad38db8b88567b274d4a15d92b03ef3
Sandeep Pawar <spawar72@in.ibm.com>
2021-05-16 12:01:27 +0000

    changed by other user

--- testApp/main.cpp
+++ testApp/main.cpp
@@ -6,9 +6,9 @@ int main(){
     cout<<"Hello World"<<std::endl;
     cout<<"Hello World"<<std::endl;
     cout<<"Hello World"<<std::endl;
-    cout<<"Hello World"<<std::endl;
     vector<int> vec = {10};
     vec.push_back(100);
+    cout<<"Hello World"<<std::endl;
     cout<<"Hello World"<<std::endl;
     cout<<"size: "<<vec.size()<<std::endl;
 }
