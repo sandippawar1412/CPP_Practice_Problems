b0c1724132358fadcb98341be9c966b4443f2fce
Sandeep Pawar <sandippawar1412@gmail.com>
2021-05-21 00:05:05 +0530

    list of includes in single header file

---
+++ Applications/includes.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+#include <fstream>
+#include <sstream>
+
+#include <iterator>
+#include <algorithm>
+#include <bitset>
+
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <deque>
+
+#include <typeinfo>
+#include <iomanip>
+
+using namespace std;
