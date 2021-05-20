b03b2df25f15ad01bca090b041dbb3319e8d3afa
Sandeep Pawar <sandippawar1412@gmail.com>
2021-05-20 23:25:02 +0530

    sample class: const, static data member, inheritance, initialization

---
+++ Applications/SampleClass.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+class Peak
+{
+    string name;
+    string color;
+};
+
+class Bird
+{
+    string name;
+    static int32_t wingCnt;
+    Peak peak; // field as a relation
+    const char tag;
+
+public:
+    void fly()
+    {
+        cout << "Bird:Fly wings" << endl;
+    }
+
+    virtual void eat() = 0;
+
+    Bird(char t) : tag(t)
+    {
+        name = "default";
+        cout << "Bird: Ctor" << endl;
+    }
+
+    ~Bird()
+    {
+        cout << "Bird: Dtor" << endl;
+    }
+
+    static void PrintMe()
+    {
+        cout << "Printing Bird Class" << endl;
+    }
+};
+
+class Parrot : public Bird
+{
+public:
+    void fly()
+    {
+        cout << "Parrot: Fly" << endl;
+        Bird::fly(); // reading base method here
+    }
+
+    void eat()
+    {
+        cout << "Parrot eating";
+    }
+
+    Parrot(char t) : Bird(t)
+    {
+        cout << "Parrot: Ctor" << endl;
+    }
+
+    ~Parrot()
+    {
+        cout << "Parrot: Dtor" << endl;
+    }
+};
+
+int32_t Bird::wingCnt = 2; // Needs to be initialized globally
+int main()
+{
+
+    //Bird b('a');
+    Parrot p('c');
+    //p.fly();
+
+    /*
+    class Parrot : public Bird then accessible
+    class Parrot :  Bird not accessible fly method
+    */
+    p.Bird::fly();
+
+    Bird::PrintMe();
+}
